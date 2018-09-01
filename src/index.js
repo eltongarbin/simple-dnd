93b32adb50beb457ca42794e02dee3f9fea2c757
Elton Nascimento <eltongarbin@gmail.com>
2018-08-31 21:08:04 -0300

    Implementation

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import AppDragDropDemo from './AppDragDropDemo';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<AppDragDropDemo />, document.getElementById('root'));
 registerServiceWorker();
